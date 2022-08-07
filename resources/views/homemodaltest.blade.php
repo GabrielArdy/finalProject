6ec268e57b64fd2dbf24bd37dd871a27c00df353
GabrielArdy <672021019@student.uksw.edu>
2022-08-07 19:59:00 +0700

    link icon

--- resources/views/homemodaltest.blade.php
+++ resources/views/homemodaltest.blade.php
@@ -5,12 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <!-- custom CSS -->
     <link rel="stylesheet" href="{{ asset('custom-style.css') }}">
     <!-- Icons -->
-    <link rel="stylesheet" href="{{ asset('icon/css/all.min.css) }}'">
+    <link rel="stylesheet" href="{{ asset('icon/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('icon/css/all.css') }}">
     <title>Home | Lembaga Kemahasiswaan</title>
 </head>
 
